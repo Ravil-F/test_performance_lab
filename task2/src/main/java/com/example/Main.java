de992f9d66330c9daeba5e71045c2231c68dc26a
rf <rf_3088@mail.ru>
2026-04-01 20:02:22 +0300

    read in file

--- task2/src/main/java/com/example/Main.java
+++ task2/src/main/java/com/example/Main.java
@@ -1,17 +1,60 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String file1 = scanPaths();
+        String file2 = scanPaths();
+        System.out.println(file1 + file2);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] cetner = new int[2];
+        int[] radius = new int[2];
+        scanFile(file1, cetner, radius);
+        for(int i = 0; i < 2; i++){
+            System.out.println("center[" + i + "] = " + cetner[i] +
+                                "radius[" + i+ "] = " + radius[i]);
         }
+    }
+
+    public static String scanPaths(){
+        boolean flag = true;
+        String file_url = new String();
+        do {
+            System.out.print("Enter file path ");
+            Scanner in = new Scanner(System.in);
+            file_url = in.nextLine();
+            File file = new File(file_url);
+            if(file.exists() && file.isFile()) {
+                flag = false;
+            }
+            else
+                System.out.println("The entered file path is incorrect");
+        }while (flag);
+
+        return file_url;
+    }
+
+    public static void scanFile(String path, int[] center, int[]radius){
+        try(Scanner scanner = new Scanner(new File(path))){
+            String line1 = scanner.nextLine();
+            String line2 = scanner.nextLine();
+            String[] res1 = line1.split(" ");
+            String[] res2 = line2.split(" ");
+
+            center = toLineInNumber(center, res1);
+            radius  = toLineInNumber(radius, res2);
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());;
+        }
+    }
+
+    private static int[] toLineInNumber(int[] array, String[] line){
+        for(int i = 0; i < 2; i++){
+            array[i] = Integer.parseInt(line[i]);
+        }
+        return array;
     }
 }
