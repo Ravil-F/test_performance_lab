ec68d60476483a5b3171ed30402927ca5803296b
rf <rf_3088@mail.ru>
2026-04-01 18:30:02 +0300

    finish task1

--- task1/src/main/java/com/example/Main.java
+++ task1/src/main/java/com/example/Main.java
@@ -1,17 +1,40 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int n, m;
+        System.out.print("Enter n: ");
+        n = scanNunb();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Enter m: ");
+        m = scanNunb();
+
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++){
+            array[i] = i + 1;
         }
+
+        String res_path = res(array, n, m);
+        System.out.println(res_path);
+    }
+
+    public static int scanNunb(){
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+
+    public static String res(int[] array, int n, int m) {
+        StringBuilder path = new StringBuilder();
+        int tmp = 0;
+
+        for (int i = 0; i < m; i++) {
+            path.append((array[tmp]));
+
+            tmp = (tmp + m - 1) % n;
+        }
+
+        return path.toString();
     }
 }
